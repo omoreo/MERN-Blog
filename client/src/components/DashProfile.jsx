068cf0758f35f5d6f0bce152235ec611c0dbe758
omoreo <nathanapatha@gmail.com>
2024-07-29 16:03:58 +0700

    user img upload fuc

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,12 +1,20 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 export default function Dashprofile() {
 
   const { currentUser } = useSelector(state => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  // console.log(imageFileUploadProgress, imageFileUploadError);
   const filePickerRef = useRef();
 
   const handleImageChange = (e) => {
@@ -23,8 +31,41 @@ export default function Dashprofile() {
   }, [imageFile]);
 
   const uploadImage = async () => {
-    console.log('uploading image');
-  }
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 * 1024 && 
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // console.log('uploading image');
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // 10.4222
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError("Couldn't upload image (File must be less than 2MB)");
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
 
   // console.log(imageFile, imageFileUrl);
   return (
@@ -38,13 +79,41 @@ export default function Dashprofile() {
           ref={filePickerRef} 
           hidden 
         />
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden" onClick={() => filePickerRef.current.click()}>
+        <div 
+          className={`relative w-32 h-32 self-center cursor-pointer overflow-hidden
+          ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opcaity-60'}`}
+          onClick={() => filePickerRef.current.click()}
+        >
+          { imageFileUploadProgress && (
+            <CircularProgressbar 
+              value={imageFileUploadProgress || 0} 
+              text={`${imageFileUploadProgress}%`} 
+              strokeWidth={2}
+              style={{
+                      root:{
+                            width: '100', 
+                            height: '100', 
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                          },
+                          path: {
+                            stroke: `rgba(62, 152, 199, ${imageFileUploadProgress / 100})`,                           
+                          },
+                    }} 
+            />
+          )}
           <img 
             src={imageFileUrl || currentUser.profilePicture} 
             alt='user-profile' 
             className="w-full h-full border-2 border-gray-800 dark:border-gray-200" 
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color='failure'>
+            {imageFileUploadError}
+          </Alert>
+        )}
         <TextInput
           type='text'
           id='username'
