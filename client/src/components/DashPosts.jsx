0280b94b80dedb05dded4f7c31d510d7d685558b
omoreo <nathanapatha@gmail.com>
2024-08-08 23:20:12 +0700

    add delete post func

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,5 +1,6 @@
-import { Table } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { useEffect, useState } from "react"
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
 
@@ -7,6 +8,8 @@ export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [PostIdToDelete, setPostIdToDelete] = useState('');
 
   console.log(userPosts);
   
@@ -42,6 +45,26 @@ export default function DashPosts() {
         if(data.posts.length < 9){
           setShowMore(false);
         }
+      }
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+
+  const handleDeletePost = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(`/api/post/deletepost/${PostIdToDelete}/${currentUser._id}`, 
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await res.json();
+      if(!res.ok){
+        console.log(data.message);
+      } else {
+        setUserPosts((prev) => prev.filter((post) => post._id !== PostIdToDelete));
       }
     } catch (error) {
       console.log(error.message);
@@ -87,7 +110,10 @@ export default function DashPosts() {
                     <Link>{post.category}</Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                    <span onClick={() => {
+                      setShowModal(true);
+                      setPostIdToDelete(post._id);
+                    }} className="font-medium text-red-500 hover:underline cursor-pointer">
                       Delete
                     </span>
                   </Table.Cell>
@@ -111,6 +137,24 @@ export default function DashPosts() {
       ) : (
         <p>You have no post yet!</p>
       )}
+      <Modal 
+        show={showModal} 
+        onClose={() => setShowModal(false)} 
+        popup 
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete post?</h3>
+            <div className="flex justify-center gap-28">
+              <Button color='failure' onClick={handleDeletePost}>Yes, I'm Sure</Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>No, cancel</Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
