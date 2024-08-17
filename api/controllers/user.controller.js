b991e3b7925c89c4963cb067e2435da852e25b0c
omoreo <nathanapatha@gmail.com>
2024-08-17 11:04:25 +0700

    delete user function for admin

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,7 +53,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id !== req.params.userId){
+    if(!req.user.isAdmin && req.user.id !== req.params.userId){
         return next(errorHandler(403, "You're not allowed to delete this user"));
     }
 
